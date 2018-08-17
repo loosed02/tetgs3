0aad476be41d06cfa82e487f43833e0f346aa274
MrZeke <30325217+charlier147@users.noreply.github.com>
2018-08-17 15:22:23 -0400

    12
    2

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require(`discord.js`);
+const auth = require(`./settings/config.json`)
+const Enmap = require("enmap");
+const fs = require(`fs`);
+const client = new Discord.Client({
+    disableEveryone: false,
+    autoReconnect: true
+});
+    //Command Handler
+     client.commands = new Enmap();
+     fs.readdir("./commands/", (err, files) => {
+     if (err) return console.error(err);
+     files.forEach(file => {
+     if (!file.endsWith(".js")) return;
+     let props = require(`./commands/${file}`);
+     let commandName = file.split(".")[0];
+     console.log(`Attempting to load command ${commandName}`);
+     client.commands.set(commandName, props );
+    });
+});
+    //Events
+    client.on('ready', () => require(`./events/ready.js`)(client));    
+    client.on('message', message => require('./events/message.js')(client, message));
+    client.on('message', message => require('./events/ticket/message.js')(client, message));
+
+ 
+client.login("")
